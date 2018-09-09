6eab24797c75eb4e8b70dbfa4074759568e4af31
rkpoorva <42445109+rkpoorva@users.noreply.github.com>
2018-09-08 21:22:33 -0700

    Add files via upload

---
+++ assignment8/substring.java
@@ -0,0 +1,14 @@
+class ss 
+{
+   public static void SubString(String str, int n)
+    {
+       for (int i = 0; i < n; i++) 
+           for (int j = i+1; j <= n; j++)
+              System.out.println(str.substring(i, j));
+    }
+   public static void main(String[] args)
+    {
+        String str = "xyz";
+        SubString(str, str.length());
+    }
+}
